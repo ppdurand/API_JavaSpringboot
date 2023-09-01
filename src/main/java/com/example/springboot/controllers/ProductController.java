06177a62daea71d4226f915f7983375336db2107
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2023-08-31 21:14:01 -0300

    metodo get by id

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -8,12 +8,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -26,9 +26,17 @@ public class ProductController {
         BeanUtils.copyProperties(productDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
-
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getAllProducts(){
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getProductById(@PathVariable(value="id") UUID id){
+        Optional<Product> product = productRepository.findById(id);
+        if(product.isEmpty())
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product.get());
     }
 }
